4ba08795ff680d5be37c7b316c7cd11641a26f7f
ArnoldGregory <omondiarnold06.com>
2026-01-05 12:30:19 +0300

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Arnold",
+    author_email="omondiarnold06@gmail.com",
+    packages=find_packages()
+)
